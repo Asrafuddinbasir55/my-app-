9406520cdebd9c31520ad3e5333e9ff8bde7cd24
Asrafuddinbasir55 <asrafuddinbasir0@gamil.com>
2022-04-02 15:03:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './Components/About/About';
 import Blogs from './Components/Blogs/Blogs';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Home from './Components/Home/Home';
+import MyChart from './Components/MyChart/MyChart';
 import Navber from './Components/Navber/Navber';
 import NotFount from './Components/NotFount/NotFount';
 import Reviews from './Components/Reviews/Reviews';
@@ -21,6 +22,7 @@ function App() {
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/mychart' element={<MyChart></MyChart>}></Route>
         <Route path='/*' element={<NotFount></NotFount>}></Route>
       </Routes>
 
---
+++ src/Components/MyChart/MyChart.js
@@ -0,0 +1,11 @@
+    import React from 'react';
+    
+    const MyChart = () => {
+        return (
+            <div>
+                <h2>this is my chart</h2>
+            </div>
+        );
+    };
+    
+    export default MyChart;
